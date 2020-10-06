c85396794be636b34935ded090baf914d3d8398c
MiriamPeskowitz <miriam5000@gmail.com>
2020-10-05 21:33:57 -0400

    Tidies user controller code

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -5,23 +5,37 @@ class Api::V1::UsersController < ApplicationController
     render json: users
   end
 
-  def create
-    user = User.create(user_params)
+  def show 
+    user = User.find(params[:id])
     render json: user
+  end 
+
+
+  def create
+    user = User.new(user_params)
+    if user.save
+      render json: user
+    else
+      render json: {errors: user.errors.full_messages}, status: :unprocessible_entity
+    end 
   end
 
   def update
     user = User.find(params[:id])
     user.update_attributes(user_params)
-    render json: user
+    if user.save
+      render json: user, status: :accepted
+    else
+      render json: {errors: user.errors.full_messages}, status: :unprocessible_entity
+    end 
   end
 
-   def destroy
+  def destroy
     User.destroy(params[:id])
   end
 
   private
   def user_params
-    params.require(:user).permit(:id, :name, :email)
+    params.require(:user).permit(:id, :name, :description)
   end 
 end 
