172203f90bcd2de653e47dc40602f25b74c4d7da
MiriamPeskowitz <miriam5000@gmail.com>
2020-10-05 20:54:39 -0400

    Comment out seeds.rb. AAdd new data thru console.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,19 @@
+# Used these for an earlier version. Current data entered by console. 
 
- 
- user = ["Rob", "Kisha", "Lulu", "Leo"]
+#  User.delete_all
+#  Entry.delete_all
 
- user.each{ |user| User.create(name: user, description: "I am #{user}. So glad to be here")}
 
-# uses the "sadness ipsum lorem" I created
+# user = ["Rob", "Kisha", "Lulu", "Leo"]
 
-entry = ["anticipation future exhausted sick panic", "confusion human empty afraid", "dazed stunned", "crushed restless "]
+# user.each{ |user| {user} = User.create(name: user, description: "I am #{user}. So glad to be here")}
 
-entry.each{ |entry| Entry.create( title: entry, content: "I'm feeling #{entry}. Anyone else feel this way?", user_id: 19, public: "yes")}
+# # uses the "sadness ipsum lorem" I created
 
-entry = ["shock", "empty", "angry", "confused and sad"]
+# entry = ["anticipation future exhausted sick panic", "confusion human empty afraid", "dazed stunned", "crushed restless "]
 
-entry.each{ |entry| Entry.create( title: entry, content: "I'm feeling #{entry}. Anyone else feel this way?", user_id: 20, public: "yes")}
+# entry.each{ |entry| Entry.create( title: entry, content: "I'm feeling #{entry}. Anyone else feel this way?", user_id: 19}
+
+# # entry = ["shock", "empty", "angry", "confused and sad"]
+
+# # entry.each{ |entry| Entry.create( title: entry, content: "I'm feeling #{entry}. Anyone else feel this way?", user_id: 20, public: "yes")}
