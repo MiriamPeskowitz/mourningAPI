b7222ac01434380dae1cf8ccc8b4379f256c130f
MiriamPeskowitz <miriam5000@gmail.com>
2021-01-14 22:56:21 -0500

    Clean up

--- app/controllers/api/v1/auth_controller.rb
+++ app/controllers/api/v1/auth_controller.rb
@@ -5,22 +5,19 @@ class Api::V1::AuthController < ApplicationController
 		@user = User.find_by(username: params[:auth][:username])	
 
 		if @user && @user.authenticate(params[:auth][:password])	
-	 
 	 		session[:user_id] = @user.id
-
 			current_user = UserSerializer.new(@user) 
-
 			render json: current_user, status: :accepted
-			
 		else
-			render json: {
+			response = {
 				error: "Invalid credentials; Login didn't work", 
 				status: :unathorized
 			}
+			render json: response
 		end 
 	end
 
-
+# autologin
 	def get_current_user
 		if logged_in?
 			render json: UserSerializer.new(current_user)
@@ -32,49 +29,11 @@ class Api::V1::AuthController < ApplicationController
 		end 
 	end 
 
+# logout
 	def destroy
-
 		session.clear
 		render json: {
 			notice: "Logged out"}, 
 			status: :ok
 	end
 end 
-
-# where do I call autologin? Also, howard calls it get_current_user? What's the most descriptive name for it? 
-	# def autologin	
-	# 	if logged_in?
-	# 		render json: {
-	# 			user: UserSerializer.new(current_user)
-	# 			}, status: :ok
-
-	# 	else
-	# 		render json: {error: "No current user"}
-	# 	end 
-	# end
-
-
-	# # if user and authenticate: generate a token 
-
-
-# end 
-	# .authenticate comes from bcrpyt 
-# private
-#     def authenticate_user
-#       user_token = request.headers['X-USER-TOKEN']
-#       if user_token
-#         @user = User.find_by_token(user_token)
-#         #Unauthorize if a user object is not returned
-#         if @user.nil?
-#           return unauthorize
-#         end
-#       else
-#         return unauthorize
-#       end
-#     end
-
-#     def unauthorize
-#       head status: :unauthorized
-#       return false
-#     end
-# end
--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,6 +1,5 @@
 class Api::V1::UsersController < ApplicationController
 
-
   def index
     @users = User.all
     render json: UserSerializer.new(@users)
@@ -16,7 +15,6 @@ class Api::V1::UsersController < ApplicationController
     if @user.save
       session[:user_id] = @user.id
       render json: UserSerializer.new(@user), status: :accepted
-     
     else
       render json: {error: "Something didn't work; try again."}, status: :unprocessible_entity
     end 
@@ -41,17 +39,3 @@ class Api::V1::UsersController < ApplicationController
     params.require(:user).permit( :username, :email,  :description, :password, :password_confirmation)
   end  
 end 
-
-
-#  later 
-#def create
-#     user = User.create(user_params)
-#     if user.valid?
-#     payload= {user_id: user.id}
-      # token = encode_token(payload)
-#       render json: UserSerializer.new(user)
-# or {user:user, jwt: token}
-#     else
-#       render json: {errors: user.errors.full_messages}, status: :unprocessible_entity
-#     end 
-#   end
